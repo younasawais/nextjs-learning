fc56223e805e655d88d258e3b3c34b4f479567c8
Younas Awais <younasawais@gmail.com>
2020-11-10 18:49:27 -0500

    Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,16 +1,21 @@
 import Layout from '../../components/layout';
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts'
 
 
 export default function Post({ postData, params }) {
-  //console.log(params)
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       {postData.title}
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
@@ -24,7 +29,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   console.log(params)
   return {
     props: {
