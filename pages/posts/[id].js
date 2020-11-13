ea6781d14209823030332535305bdbbfcdc54526
Younas Awais <younasawais@gmail.com>
2020-11-13 09:48:56 -0500

    Date, ulits.css added, hello.js api test

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,8 @@
 import Layout from '../../components/layout';
 import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts'
-
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData, params }) {
   return (
@@ -9,13 +10,13 @@ export default function Post({ postData, params }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
